9cb5da94e74a8204608b7a31fe3f47dba3953a6e
MatthiasDesmoulins <Matthias-Externe.Desmoulins@asmeg.org>
2024-04-16 07:43:31 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Dictionnary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char,string> Dictionary = new Dictionary<char, string>();
+            Dictionary['a'] = "aka";
+            Dictionary['b'] = "bouloulou";
+            Dictionary['c'] = "calafacarate";
+            Dictionary.Remove('c');
+
+        }
+    }
+}
